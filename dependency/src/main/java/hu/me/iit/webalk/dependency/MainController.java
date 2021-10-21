921ef4b5c79dce21ff3570c5253864e969b7b1fd
Csiszár Levente <cs.lev007@gmail.com>
2021-10-21 22:26:29 +0200

    add controller

---
+++ dependency/src/main/java/hu/me/iit/webalk/dependency/MainController.java
@@ -0,0 +1,22 @@
+package hu.me.iit.webalk.dependency;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	private Service service;
+
+    @Autowired
+    @Qualifier("NeededService")
+    public void setService(Service service){
+        this.service = service;
+    }
+
+    @RequestMapping("/")
+    public int returnFive(){
+        return service.returnFive();
+    }
+}
