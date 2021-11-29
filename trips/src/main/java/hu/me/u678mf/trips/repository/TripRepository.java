508b079d67b385163f05a2ee53137aa23434162f
cslevi007 <79060692+cslevi007@users.noreply.github.com>
2021-11-29 17:57:23 +0100

    Delete trips directory

--- trips/src/main/java/hu/me/u678mf/trips/repository/TripRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.me.u678mf.trips.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TripRepository extends PagingAndSortingRepository<TripEntity, Long>{
-	
-}
